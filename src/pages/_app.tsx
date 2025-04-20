01b78749a39f77b9b6ddb2bf0f096e397efcbd9b
chenhaos112312 <158687105+chenhaos112312@users.noreply.github.com>
2025-04-20 17:44:47 +0800

    Add files via upload

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import { AppPropsWithLayout } from "../types"
 import { Hydrate, QueryClientProvider } from "@tanstack/react-query"
 import { RootLayout } from "src/layouts"
 import { queryClient } from "src/libs/react-query"
+// @ts-ignore
+import 'src/styles/globals.css'
 
 function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout || ((page) => page)
